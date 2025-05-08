4e3a1de1b558a32c5c1f09563cb8dbe6243334c9
Laurent Senta <laurent@singulargarden.com>
2025-05-08 16:49:49 +0200

    test: add temporary placeholder tests

---
+++ libs/backend/shared/database/src/smoke.spec.ts
@@ -0,0 +1,7 @@
+// A smoke test to skip the "no test found" error until we actually test this part.
+
+describe('Smoke Test', () => {
+  it('should run a smoke test', () => {
+    expect(true).toBe(true);
+  });
+});
